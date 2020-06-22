8505200a5b1bac344fe64b51e3a5a00db05d2b85
ejedrzejczyk <emilia.jedrzejczyk.123@gmail.com>
2020-06-22 20:56:09 +0200

    Initial commit

---
+++ MyArray.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Tablica
+{
+    class MyArray
+    {
+        private int[,] array;
+
+        public delegate void ChangedEventHandler(int firstDimension, int secondDimenson);
+        public event ChangedEventHandler ArraySizeChanged;
+
+        protected virtual void OnArraySizeChanged(int firstDimension, int secondDimenson)
+        {
+            if(ArraySizeChanged != null)
+            {
+                ArraySizeChanged(firstDimension, secondDimenson);
+            }
+        }
+
+        public int[] Size
+        {
+            get
+            {
+                int[] s = new int[2];
+                s[0] = array.GetLength(0);
+                s[1] = array.GetLength(1);
+                return s;
+            }
+        }
+
+        public MyArray(int firstDimension, int secondDimension)
+        {
+            array = new int[firstDimension, secondDimension];
+        }
+
+        public int this[int firstIndex,int secondIndex]
+        {
+            get
+            {
+                return ReadValue(firstIndex, secondIndex);
+            }
+            set
+            {
+                WriteValue(value, firstIndex, secondIndex);
+            }
+        }
+
+        public void PrintArray()
+        {
+            Console.WriteLine("\n");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+        }
+
+        public int ReadValue(int firstIndex, int secondIndex)
+        {
+            try
+            {
+                return (int)array.GetValue(firstIndex, secondIndex);
+
+            }
+            catch(IndexOutOfRangeException)
+            {
+                throw new IndexOutOfRangeException("Przekroczenie rozmiaru tablicy\nRozmiar tablicy: " + Size[0] + "x" + Size[1]);
+            }
+        }
+
+        public void WriteValue(int value, int firstIndex, int secondIndex)
+        {
+            int newFirstSize = array.GetLength(0);
+            int newSecondSize = array.GetLength(1);
+
+            try
+            {
+                array.SetValue(value, firstIndex, secondIndex);
+            }
+            catch(IndexOutOfRangeException)
+            {
+                if (firstIndex + 1> array.GetLength(0))
+                {
+                    newFirstSize = firstIndex + 1;
+                }
+                if (secondIndex + 1> array.GetLength(1))
+                {
+                    newSecondSize = secondIndex + 1;
+                }
+
+                int[,] newArray = new int[newFirstSize, newSecondSize];
+
+                for(int i = 0; i<array.GetLength(0); i++)
+                {
+                    for(int j = 0; j<array.GetLength(1); j++)
+                    {
+                        newArray[i, j] = array[i, j];
+                    }
+                }
+
+                newArray.SetValue(value, firstIndex, secondIndex);
+                array = newArray;
+                OnArraySizeChanged(Size[0], Size[1]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace Tablica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstDim;
+            int secondDim;
+
+            Console.WriteLine("Tworzenie nowej tablicy");
+            Console.WriteLine("\nWprowadź pierwszy wymiar:");
+            firstDim = InputDimension();
+            Console.WriteLine("\nWprowadź drugi wymiar:");
+            secondDim = InputDimension();
+            MyArray array = new MyArray(firstDim, secondDim);
+            array.ArraySizeChanged += ArrayChangedLog;
+
+            while (true)
+            {
+                Console.WriteLine("\nWcisnij 'R' aby odczytac wartosc z tablicy lub 'W' aby zapisac liczbe do tablicy");
+                Console.WriteLine("\nWcisnij 'P' aby wypisac cala tablice");
+                Console.WriteLine("\nWcisnij 'ESC' aby wylaczyc aplikacje\n");
+                ConsoleKeyInfo cki = Console.ReadKey();
+
+                if (cki.Key == ConsoleKey.P)
+                {
+                    array.PrintArray();
+                }
+                else if (cki.Key == ConsoleKey.R)
+                {
+                    try
+                    {
+                        int i = InputIndex();
+                        int j = InputIndex();
+                        int val = array[i, j];
+                        Console.WriteLine("\nElement [{0},{1}] = {2}", i, j, val);
+                    }
+                    catch(IndexOutOfRangeException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else if (cki.Key == ConsoleKey.W)
+                {
+                    array[InputIndex(), InputIndex()] = InputValue();
+
+                }
+                else if (cki.Key == ConsoleKey.Escape)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("\nUnexpected key");
+                }
+            }
+        }
+
+        private static void ArrayChangedLog(int firstDimension, int secondDimension)
+        {
+            Console.WriteLine("\nTablica rozszerzona.");
+            Console.WriteLine("Aktualny rozmiar: {0}x{1}", firstDimension, secondDimension);
+        }
+
+        public static int InputDimension()
+        {
+            int dimension;
+            while (true)
+            {
+               //Console.WriteLine("\nWprowadź pierwszy wymiar:");
+                try
+                {
+                    do
+                    {
+                        dimension = Convert.ToInt32(Console.ReadLine());
+                        if (dimension < 0) Console.WriteLine("Dozwolone tylko dodatnie wartosci!\n");
+                    }
+                    while (dimension < 0);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Niepoprawny format liczby!\n");
+                    continue;
+                }
+            }
+            return dimension;
+        }
+
+        public static int InputIndex()
+        {
+            int index;
+            while (true)
+            {
+                Console.WriteLine("\nWprowadź index tablicy:");
+                try
+                {
+                    do
+                    {
+                        index = Convert.ToInt32(Console.ReadLine());
+                        if (index < 0) Console.WriteLine("Dozwolone tylko dodatnie wartosci!\n");
+                    }
+                    while (index < 0);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Niepoprawny format liczby!\n");
+                    continue;
+                }
+            }
+            return index;
+        }
+
+        public static int InputValue()
+        {
+            int value;
+            while (true)
+            {
+                Console.WriteLine("\nWprowadź wartość do tablicy:\n");
+                try
+                {
+                    value = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Niepoprawny format liczby!\n");
+                    continue;
+                }
+            }
+            return value;
+        }
+    }
+}
